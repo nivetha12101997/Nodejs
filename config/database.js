ae5fe009118bd822325641113d2c24770fbad806
nivetha <nivethabe1997@gmail.com>
2025-04-23 17:31:21 +0530

    Added Database config

---
+++ config/database.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+const config={
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+};
+
+const pool = mysql.createPool(config).promise();
+
+module.exports={
+    pool,
+    query:(text,params)=>pool.query(text,params)
+}
