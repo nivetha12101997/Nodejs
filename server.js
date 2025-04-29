918ceab5b95ea4a118ed4845f18abd1f0ef1c670
nivetha <nivethabe1997@gmail.com>
2025-04-29 19:14:12 +0530

    corrected the CORS origin url

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
 const corsOptions = {
-    origin: ['http://localhost:5173','http://trilokinnovation.com','https://trilokinnovation.com'],
+    origin: ['http://localhost:5173','http://trilokinnovations.com','https://trilokinnovations.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   };
 app.use(cors(corsOptions));
